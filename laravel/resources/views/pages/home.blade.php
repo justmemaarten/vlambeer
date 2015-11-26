bfecfa9b01a923d87c47b9725e730d85f53341a4
ateck5 <jitsbuijs@hotmail.com>
2015-11-26 10:10:17 +0100

    accordion doet het

--- laravel/resources/views/pages/home.blade.php
+++ laravel/resources/views/pages/home.blade.php
@@ -1,50 +1,92 @@
 @extends('base')
 
 @section('content')
-    <div class="row row-game-item">
-        <div class="game-item col-md-4"><div class="content"><a class="btn btn-primary" role="button" data-toggle="collapse" href="#gunGodz" aria-expanded="false" aria-controls="collapseExample"><img src="http://www.lorempixel.com/200/200" alt=""></a></div></div>
-        <div class="game-item col-md-4"><div class="content"><a class="btn btn-primary" role="button" data-toggle="collapse" href="#lufTrausers" aria-expanded="false" aria-controls="collapseExample"><img src="http://www.lorempixel.com/200/200" alt=""></a></div></div>
-        <div class="game-item col-md-4"><div class="content"><a class="btn btn-primary" role="button" data-toggle="collapse" href="#nuclear-Throne" aria-expanded="false" aria-controls="collapseExample"><img src="http://www.lorempixel.com/200/200" alt=""></a></div></div>
-    </div>
+    <div class="panel-group" id="accordion">
+        <div class="panel panel-default">
+            <div class="row row-game-item">
 
-    {{--these are the texts wich are being dropped down when you click on a link--}}
-    <div class="collapse" id="gunGodz">
-        <div class="well gunGodz">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci at blanditiis consequuntur cupiditate delectus et eum exercitationem
-                explicabo fuga inventore iure magnam, minus mollitia pariatur, perferendis perspiciatis rem repellat tempora.</p>
-        </div>
-    </div>
-    <div class="collapse" id="lufTrausers">
-        <div class="well lufTrausers">
-            <p>test test</p>
-        </div>
-    </div>
-    <div class="collapse" id="nuclear-Throne">
-        <div class="well nuclear-Throne">
-            <p>test test</p>
-        </div>
-    </div>
-    <div class="row row-game-item">
-        <div class="game-item col-md-4"><div class="content"><a class="btn btn-primary" role="button" data-toggle="collapse" href="#ridiculous-Fishing" aria-expanded="false" aria-controls="collapseExample"><img src="http://www.lorempixel.com/200/200" alt=""></a></div></div>
-        <div class="game-item col-md-4"><div class="content"><a class="btn btn-primary" role="button" data-toggle="collapse" href="#serious-Sam" aria-expanded="false" aria-controls="collapseExample"><img src="http://www.lorempixel.com/200/200" alt=""></a></div></div>
-        <div class="game-item col-md-4"><div class="content"><a class="btn btn-primary" role="button" data-toggle="collapse" href="#super-Crate-Box" aria-expanded="false" aria-controls="collapseExample"><img src="http://www.lorempixel.com/200/200" alt=""></a></div></div>
-    </div>
+                <div class="game-item col-md-4">
+                    <div class="content panel-title">
+                        <a class="btn btn-primary" data-toggle="collapse" data-parent="#accordion" href="#gunGodz">
+                            <img src="http://www.lorempixel.com/200/200" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="game-item col-md-4 panel-heading">
+                    <div class="content panel-title">
+                        <a class="btn btn-primary" data-toggle="collapse" data-parent="#accordion" href="#lufTrausers">
+                            <img src="http://www.lorempixel.com/200/200" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="game-item col-md-4 panel-heading">
+                    <div class="content panel-title">
+                        <a class="btn btn-primary" data-toggle="collapse" data-parent="#accordion" href="#nuclear-Throne">
+                            <img src="http://www.lorempixel.com/200/200" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
 
-    {{--these are the texts wich are being dropped down when you click on a link--}}
-    <div class="collapse" id="ridiculous-Fishing">
-        <div class="well ridiculous-Fishing">
-            <p>test test</p>
+            {{--these are the texts which are being dropped down when you click on a link--}}
+            <div class="panel-collapse collapse" id="gunGodz">
+                <div class="panel-body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci at blanditiis consequuntur cupiditate delectus et eum exercitationem
+                        explicabo fuga inventore iure magnam, minus mollitia pariatur, perferendis perspiciatis rem repellat tempora.</p>
+                </div>
+            </div>
+            <div class="collapse collapse" id="lufTrausers">
+                <div class="panel-body">
+                    <p>test test</p>
+                </div>
+            </div>
+            <div class="collapse collapse" id="nuclear-Throne">
+                <div class="panel-body">
+                    <p>test test</p>
+                </div>
+            </div>
+            <div class="row row-game-item">
+                <div class="game-item col-md-4">
+                    <div class="content panel-title">
+                        <a class="btn btn-primary" data-toggle="collapse" data-parent="#accordion" href="#ridiculous-Fishing">
+                            <img src="http://www.lorempixel.com/200/200" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="game-item col-md-4">
+                    <div class="content panel-title">
+                        <a class="btn btn-primary" data-toggle="collapse" data-parent="#accordion" href="#serious-Sam">
+                            <img src="http://www.lorempixel.com/200/200" alt="">
+                        </a>
+                    </div>
+                </div>
+                <div class="game-item col-md-4">
+                    <div class="content panel-title">
+                        <a class="btn btn-primary" data-toggle="collapse" data-parent="#accordion" href="#super-Crate-Box">
+                            <img src="http://www.lorempixel.com/200/200" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            {{--these are the texts which are being dropped down when you click on a link--}}
+            <div class="panel-collapse collapse" id="ridiculous-Fishing">
+                <div class="panel-body">
+                    <p>test test</p>
+                </div>
+            </div>
+            <div class="panel-collapse collapse" id="serious-Sam">
+                <div class="panel-body">
+                    <p>test test</p>
+                </div>
+            </div>
+            <div class="panel-collapse collapse" id="super-Crate-Box">
+                <div class="panel-body">
+                    <p>test test</p>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="collapse" id="serious-Sam">
-        <div class="well serious-Sam">
-            <p>test test</p>
-        </div>
-    </div>
-    <div class="collapse" id="super-Crate-Box">
-        <div class="well super-Crate-Box">
-            <p>test test</p>
-        </div>
+
     </div>
 
     <a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
