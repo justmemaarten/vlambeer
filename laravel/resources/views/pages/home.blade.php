f8ea9d26f08837109f540e18914dfee90167c15d
ateck5 <jitsbuijs@hotmail.com>
2015-11-27 11:10:29 +0100

    kleuren fixed ish

--- laravel/resources/views/pages/home.blade.php
+++ laravel/resources/views/pages/home.blade.php
@@ -7,22 +7,22 @@
 
                 <div class="game-item col-md-4 panel-heading">
                     <div class="content panel-title">
-                        <a class="btn btn-primary" data-toggle="collapse" data-parent="#accordion" href="#gunGodz">
-                            <img src="http://www.lorempixel.com/200/200" alt="">
+                        <a class="btn btn-primary game-img" data-toggle="collapse" data-parent="#accordion" href="#gunGodz">
+                            <img src="http://www.lorempixel.com/350/200" alt="">
                         </a>
                     </div>
                 </div>
                 <div class="game-item col-md-4 panel-heading">
                     <div class="content panel-title">
-                        <a class="btn btn-primary" data-toggle="collapse" data-parent="#accordion" href="#lufTrausers">
-                            <img src="http://www.lorempixel.com/200/200" alt="">
+                        <a class="btn btn-primary game-img" data-toggle="collapse" data-parent="#accordion" href="#lufTrausers">
+                            <img src="http://www.lorempixel.com/350/200" alt="">
                         </a>
                     </div>
                 </div>
                 <div class="game-item col-md-4 panel-heading">
                     <div class="content panel-title">
-                        <a class="btn btn-primary" data-toggle="collapse" data-parent="#accordion" href="#nuclear-Throne">
-                            <img src="http://www.lorempixel.com/200/200" alt="">
+                        <a class="btn btn-primary game-img" data-toggle="collapse" data-parent="#accordion" href="#nuclear-Throne">
+                            <img src="http://www.lorempixel.com/350/200" alt="">
                         </a>
                     </div>
                 </div>
@@ -34,12 +34,12 @@
                     <p><a href="{{ action("PagesController@product",'id=6') }}">Gun Godz</a></p>
                 </div>
             </div>
-            <div class="collapse collapse" id="lufTrausers">
+            <div class="panel-collapse collapse" id="lufTrausers">
                 <div class="panel-body">
                     <p>test test</p>
                 </div>
             </div>
-            <div class="collapse collapse" id="nuclear-Throne">
+            <div class="panel-collapse collapse" id="nuclear-Throne">
                 <div class="panel-body">
                     <p>test test</p>
                 </div>
@@ -47,22 +47,22 @@
             <div class="row row-game-item">
                 <div class="game-item col-md-4 panel-heading">
                     <div class="content panel-title">
-                        <a class="btn btn-primary" data-toggle="collapse" data-parent="#accordion" href="#ridiculous-Fishing">
-                            <img src="http://www.lorempixel.com/200/200" alt="">
+                        <a class="btn btn-primary game-img" data-toggle="collapse" data-parent="#accordion" href="#ridiculous-Fishing">
+                            <img src="http://www.lorempixel.com/350/200" alt="">
                         </a>
                     </div>
                 </div>
                 <div class="game-item col-md-4 panel-heading">
                     <div class="content panel-title">
-                        <a class="btn btn-primary" data-toggle="collapse" data-parent="#accordion" href="#serious-Sam">
-                            <img src="http://www.lorempixel.com/200/200" alt="">
+                        <a class="btn btn-primary game-img" data-toggle="collapse" data-parent="#accordion" href="#serious-Sam">
+                            <img src="http://www.lorempixel.com/350/200" alt="">
                         </a>
                     </div>
                 </div>
                 <div class="game-item col-md-4 panel-heading">
                     <div class="content panel-title">
-                        <a class="btn btn-primary" data-toggle="collapse" data-parent="#accordion" href="#super-Crate-Box">
-                            <img src="http://www.lorempixel.com/200/200" alt="">
+                        <a class="btn btn-primary game-img" data-toggle="collapse" data-parent="#accordion" href="#super-Crate-Box">
+                            <img src="http://www.lorempixel.com/350/200" alt="">
                         </a>
                     </div>
                 </div>
