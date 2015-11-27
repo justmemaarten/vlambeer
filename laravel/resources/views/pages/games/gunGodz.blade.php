82e60ea00c8f6fd3682cbd16afc53410963d9389
Maarten de Wolf <pilootwolfje@hotmail.com>
2015-11-27 10:53:04 +0100

    kleine update gamepage

--- laravel/resources/views/pages/games/gunGodz.blade.php
+++ laravel/resources/views/pages/games/gunGodz.blade.php
@@ -1,7 +1,18 @@
 @extends('base')
 
 @section('content')
+    <div class="row">
+        <div class="col-md-3 game-img">
+            <img src="http://lorempixel.com/250/250/" alt="pixel">
+        </div>
+        <div class="col-md-9 game-info">
+            <h1>Gun Godz</h1>
+            <h4>Electronic arts &nbsp;</h4>
+            <h4>Sportgames &nbsp;</h4>
+            <h4>Actie en avontuur &nbsp;</h4>
+            <h4>XXXXXX &nbsp;</h4>
+        </div>
+    </div>
 
-    
-<h1>Gun Godz</h1>
+
 @stop
