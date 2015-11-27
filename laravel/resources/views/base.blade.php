aed69875c0da449e0c82157d46a9b5e8324b9c2e
ateck5 <jitsbuijs@hotmail.com>
2015-11-27 10:03:32 +0100

    fixen modal

--- laravel/resources/views/base.blade.php
+++ laravel/resources/views/base.blade.php
@@ -49,41 +49,42 @@
         </div>
 
 
-        <!-- Modal -->
-        <div class="modal fade" id="login-register" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                    </div>
-                    <div>
+    </header>
+    
+    <!-- Modal -->
+    <div class="modal fade" id="login-register" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>
+                <div>
 
-                        <!-- Nav tabs -->
-                        <ul class="nav nav-tabs" role="tablist">
-                            <li role="presentation" class="active"><a href="#login" aria-controls="login" role="tab" data-toggle="tab">Log in</a></li>
-                            <li role="presentation"><a href="#register" aria-controls="register" role="tab" data-toggle="tab">Register</a></li>
-                        </ul>
+                    <!-- Nav tabs -->
+                    <ul class="nav nav-tabs" role="tablist">
+                        <li role="presentation" class="active"><a href="#login" aria-controls="login" role="tab" data-toggle="tab">Log in</a></li>
+                        <li role="presentation"><a href="#register" aria-controls="register" role="tab" data-toggle="tab">Register</a></li>
+                    </ul>
 
-                        <!-- Tab panes -->
-                        <div class="tab-content">
-                            <div role="tabpanel" class="tab-pane active" id="login">
-                                @yield('pages/login')
-                            </div>
-                            <div role="tabpanel" class="tab-pane" id="register">
-
-                            </div>
+                    <!-- Tab panes -->
+                    <div class="tab-content">
+                        <div role="tabpanel" class="tab-pane active" id="login">
+                            @yield('pages/login')
                         </div>
+                        <div role="tabpanel" class="tab-pane" id="register">
 
+                        </div>
                     </div>
-                    <div class="modal-body">
 
-                    </div>
-                    <div class="modal-footer">
-                    </div>
+                </div>
+                <div class="modal-body">
+
+                </div>
+                <div class="modal-footer">
                 </div>
             </div>
         </div>
-    </header>
+    </div>
     <div class="container">
         <div id="quote"></div>
 
