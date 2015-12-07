89d62a7011945ba11ab39acd45ad667a33f95382
Damian Leijten <damian@marcleijten.nl>
2015-12-07 10:07:19 +0100

    shopping cart in progress

--- laravel/resources/views/pages/shop/cart.blade.php
+++ laravel/resources/views/pages/shop/cart.blade.php
@@ -5,9 +5,18 @@
 
 
     <h1>Shopping Cart</h1>
-    <div class="product col-md-4"></div>
-    <div class="product col-md-4"></div>
-    <div class="product col-md-4"></div>
+    <div class="product col-md-3">
+        <div class="product-price"></div>
+    </div>
+    <div class="product col-md-3">
+        <div class="product-price"></div>
+    </div>
+    <div class="product col-md-3">
+        <div class="product-price"></div>
+    </div>
+
+
+    <input class="btn btn-primary" type="submit">
 
 
     @stop
