2d79dc6008eab471f039a50c49cedfca77d54b8d
Evelien Rookmaker <Studie@rookmaker.nl>
2015-12-07 12:27:09 +0100

    Functies geschreven
    BTW functie en totaal prijs functie toegevoegd.

---
+++ laravel/app/Http/Controllers/PaymentController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class PaymentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create_price_product($product_id)
+    {
+        $price = mysql_query(SELECT price FROM tbl_products WHERE product_id = $product_id);
+            //calculate sales tax
+        $btw = ($price * 0.21);
+        $pricebtw = ($price * 1.21);
+
+    }
+    public function create_total_price($product_id)
+    {
+        $price = mysql_query(SELECT price FROM tbl_products WHERE product_id = $product_id);
+            //calculate sales tax
+        $pricebtw = ($price * 1.21);
+
+        $totalprice = SUM($price);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //btw tonen
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
