a36f5910db75ad1597e1b1289c5fc0cdbbca6e86
Damian Leijten <damian@marcleijten.nl>
2015-12-07 11:22:24 +0100

    cart page af

--- laravel/resources/views/pages/shop/cart.blade.php
+++ laravel/resources/views/pages/shop/cart.blade.php
@@ -5,18 +5,38 @@
 
 
     <h1>Shopping Cart</h1>
-    <div class="product col-md-3">
-        <div class="product-price"></div>
-    </div>
-    <div class="product col-md-3">
-        <div class="product-price"></div>
-    </div>
-    <div class="product col-md-3">
-        <div class="product-price"></div>
+    <div class="row row-shopping-cart">
+        <div class="product col-md-3">
+            <img class="cart-img" src="http://www.lorempixel.com/329/230" alt="">
+            <div class="product-price">
+                <h4>Product info</h4>
+            </div>
+        </div>
+        <div class="product col-md-3">
+            <img class="cart-img" src="http://www.lorempixel.com/329/230" alt="">
+            <div class="product-price">
+                <h4>Product info</h4>
+            </div>
+        </div>
+        <div class="product col-md-3">
+            <img class="cart-img" src="http://www.lorempixel.com/329/230" alt="">
+            <div class="product-price">
+                <h4>Product info</h4>
+            </div>
+        </div>
     </div>
 
 
-    <input class="btn btn-primary" type="submit">
+    <div class="total-price">
+        <div class="price-info">
+            <p>Totaal aantal artikelen (<!-- Het totaal aantal producten ophalen via blade -->)</p>
+            <p>verzendkosten (<!-- Verzendkosten berekenen via blade indien nodig -->)</p>
+            <p>Totaal (<!-- Het totaal berekenen via blade -->)</p>
+        </div>
+    </div>
+
+    <input class="btn btn-primary pull-right pay-button" type="submit">
+
 
 
     @stop
