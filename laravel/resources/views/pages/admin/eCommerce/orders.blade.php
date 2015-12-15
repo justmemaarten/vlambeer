0f1099320adbe462650cffdcbdfc6609f3d0780d
Damian Leijten <damian@marcleijten.nl>
2015-12-15 12:22:36 +0100

    kleine aanpassing aan orders.blade.php

--- laravel/resources/views/pages/admin/eCommerce/orders.blade.php
+++ laravel/resources/views/pages/admin/eCommerce/orders.blade.php
@@ -2,7 +2,31 @@
 
 @section('content')
 
-
+<div class="main-content">
+    <ul>
+        <li><img src="" alt=""></li>
+        <li>ID</li>
+        <li>Catergory</li>
+        <li>Date created</li>
+        <li>Price</li>
+        <li>Status</li>
+        <li>edit/delete</li>
+    </ul>
+    @foreach ($customer as $customers)
+        <ul>
+            <li>{{Auth::user()->id}}</li>
+            <li></li>
+            <li>{{Auth::user()->created_at}}</li>
+            <li></li>
+            <li></li>
+            <li>{{Auth::user()->status}}</li>
+            <li>
+                <input type="submit" value="edit">
+                <input type="submit" value="delete">
+            </li>
+        </ul>
+    @endforeach
+</div>
 
 
 @stop
