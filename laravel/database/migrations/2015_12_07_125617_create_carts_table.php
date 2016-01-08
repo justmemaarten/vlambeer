ad94c955c2db2df99d505e08238077f3daf80cb7
Maarten de Wolf <pilootwolfje@hotmail.com>
2016-01-08 11:01:50 +0100

    migrate

--- laravel/database/migrations/2015_12_07_125617_create_carts_table.php
+++ laravel/database/migrations/2015_12_07_125617_create_carts_table.php
@@ -15,12 +15,14 @@ class CreateCartsTable extends Migration
 
         $this->down();
         Schema::create('tbl_cart', function(Blueprint $table){
-            $table->integer('id', false, true);
-            $table->foreign('id')->references('id')->on('tbl_users');
+            $table->integer('id');
+            $table->integer('user_id', false, true);
+            $table->foreign('user_id')->references('id')->on('tbl_users');
             $table->integer('product_id', false, true);
             $table->foreign('product_id')->references('product_id')->on('tbl_products');
             $table->integer('amount', false);
             $table->dateTime('paid');
+            $table->string('status', 255);
             $table->timestamps();
         });
     }
