0d696dbbd293f05af3cc081565bd192d025027e6
ateck5 <jitsbuijs@hotmail.com>
2016-01-12 11:24:26 +0100

    product show + cart blade fixed

--- laravel/resources/views/pages/products/show.blade.php
+++ laravel/resources/views/pages/products/show.blade.php
@@ -4,5 +4,21 @@
     <h1>{{ $product['name']  }}</h1>
     <h2>Stock: {{ $product['stock'] }}</h2>
 
+    <form class="form-horizontal" action="{{action("CartController@store")}}" method="post">
+        {{ csrf_field() }}
+        <input type="hidden" name="id" value="{{$product['product_id']}}">
+        <div class="form-group">
+            <label class="control-label">Amount</label>
+            <div class="input-group">
+                <span class="input-group-addon">#</span>
+                <input type="number" class="form-control" min="1" value="1" name="amount">
+                <span class="input-group-btn">
+                    <input type="submit" class="btn btn-default" value="Add to cart">
+                    {{--<button class="btn btn-default" type="button">Add to cart</button>--}}
+                </span>
+            </div>
+        </div>
+    </form>
+
 
 @stop
--- laravel/resources/views/pages/shop/cart.blade.php
+++ laravel/resources/views/pages/shop/cart.blade.php
@@ -3,69 +3,99 @@
 @section('content')
 
     <h1>Shopping Cart</h1>
-    <div class="row row-shopping-cart">
+    @if(isset($products))
+        <div class="row row-shopping-cart">
+            <table class="table table-striped table-hover col-md-12">
+                @foreach ($products as $product => $data)
+                    @if(isset($products[$product]['product_id']))
+                        {{ $product['product_id'] }}
+                        <tr class="cartproduct">
+                            <td class="pull-left">
+                                <form action="{{action("CartController@destroy", $products[$product]['product_id'])}}" method="post">
+                                    <input type="hidden" name="_method" value="delete">
+                                    {{csrf_field()}}
+                                    <input type="submit" class="btn btn-primary" value="Delete">
+                                </form>
+                            </td>
+                            <td class="pull-left">
+                                <img src="{{$data['img']}}" alt="">
+                            </td>
+                            <td class="pull-left">
+                                <h3>{{$data['name']}}</h3>
+                            </td>
+                            <td class="pull-right price">
+                                <p class="product-price">
+                                    &#8364; {{$data['price']}}
+                                </p>
+                            </td>
+                            <td class="pull-right amount">
+                                <form action="{{action("CartController@update", $products[$product]['product_id'])}}" method="post">
+                                    <input type="hidden" name="_method" value="put">
+                                    {{ csrf_field() }}
 
-        @foreach ($products as $product => $data)
-            @if(isset($products[$product]['product_id']))
-            <div class="product col-md-3">
-                <img class="cart-img" src="{{$data['img']}}" alt="">
-                <p class="product-amount">{{$data['amount']}} x</p>
+                                    <div class="input-group">
+                                        <span class="input-group-addon">#</span>
+                                        <input type="number" class="form-control" min="1" value="{{$data['amount']}}" name="amount">
+                                    <span class="input-group-btn">
+                                        <input type="submit" class="btn btn-default" value="Edit amount">
+                                    </span>
+                                    </div>
+                                </form>
+                            </td>
+                        </tr>
 
-
-                <div class="form-group delete-cart-item">
-                    <div class="input-group">
-                        <input class="form-control" type="number" min="0" max="{{$data['amount']}}">
-                        <span class="input-group-btn">
-                          <input class="btn btn-primary" type="submit" value="Delete">
-                        </span>
-                    </div>
-                </div>
-
-                <div class="product-info">
-                    <h4>Product info</h4>
-                    <p class="product-price">
-                        &#8364; {{$data['price']}}
-                    </p>
-                </div>
-            </div>
-
-            @endif
-        @endforeach
-
-    </div>
-
-
-    <div class="total-price">
-        <div class="price-info">
-            <p>Totaal prijs excl.({{$products['total']}} artikelen)</p>
-            <p>&#8364; {{$products['totalprice']}}</p>
-            <br>
-            <p>BTW({{round((float)$products['btw'] * 100 )}}%)</p>
-            <p>&#8364; {{$products['totalprice'] * $products['btw']}}</p>
-            <br>
-            <p>Totaal prijs incl.</p>
-            <p>&#8364; {{$products['totalprice'] * (1+$products['btw'])}}</p>
-        </div>
-    </div>
-
-   <a href="{{ action("PagesController@data") }}" class="btn btn-primary pull-right pay-button" type="submit">Next</a>
-
-    @if(count($products))
-        <div class="suggestions">
-            <h2>Maybe you'd like:</h2>
-            <div class="row">
-                @foreach($suggestions as $suggestion)
-                    <div class="col-md-2 suggestion">
-                        <img src="{{$suggestion['img']}}" alt="">
-
-                        <div class="suggestion-info">
-                            <h4>{{$suggestion['name']}}</h4>
-                        </div>
-                    </div>
+                    @endif
                 @endforeach
+            </table>
+
+
+        </div>
+
+
+        <div class="total-price">
+            <div class="price-info">
+                <p>Totaal prijs excl.({{$products['total']}} artikelen)</p>
+                <p>&#8364; {{$products['totalprice']}}</p>
+                <br>
+                <p>BTW({{round((float)$products['btw'] * 100 )}}%)</p>
+                <p>&#8364; {{$products['totalprice'] * $products['btw']}}</p>
+                <br>
+                <p>Totaal prijs incl.</p>
+                <p>&#8364; {{$products['totalprice'] * (1+$products['btw'])}}</p>
             </div>
+        </div>
+        @if (Auth::check())
+            <a href="{{ action("PagesController@data") }}" class="btn btn-primary pull-right pay-button" type="submit">Next</a>
+        @else
+            <div class="pull-right">
+                <h4>Please <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#login-register">
+                        Login
+                    </button> to order products.</h4>
+            </div>
+        @endif
+
+        @if(count($products))
+            <div class="suggestions">
+                <h2>Maybe you'd like:</h2>
+                <div class="row">
+                    @foreach($suggestions as $suggestion)
+                        <div class="col-md-2 suggestion">
+                            <img src="{{$suggestion['img']}}" alt="">
+
+                            <div class="suggestion-info">
+                                <h4>{{$suggestion['name']}}</h4>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        @endif
+    @else
+        <div class="row row-shopping-cart">
+            <h4>There are no items in your cart! please head to the <a href="{{action('ProductController@index')}}">store</a> to add items to your shopping cart.</h4>
         </div>
     @endif
+
 
 
 @stop
