0bd86733bcb4980da61c1d7143f1c925827010be
DavidvdBurgh <davidvdburgh@live.nl>
2016-01-12 11:58:41 +0100

    about (af)

--- laravel/resources/views/pages/about.blade.php
+++ laravel/resources/views/pages/about.blade.php
@@ -2,11 +2,12 @@
 
 @section('content')
 
-        <h1>About</h1>
+        <h1>About Vlambeer</h1>
         <p>Vlambeer was founded in 2010 by Rami Ismail and Jan Willem Nijman after both dropped out from a game design course at the Utrecht School of the Arts. Ismail and Nijman began by working together to develop a prototype, initially created by Nijman, called "Crates from Hell". It was released on 11 May 2010 as Super Crate Box and earned the studio recognition in the form of an Independent Games Festival finalist position in the Excellence in Design category. During the development of Super Crate Box, Vlambeer developed Radical Fishing. It was released in November 2010. Radical Fishing was the first of many games released by the studio under a "Not Vlambeer" label, which encompasses the studio's games that are developed for money or as an experiment.
             Vlambeer was then approached by publisher Devolver Digital to develop a game in the Serious Sam franchise. The studio and Devolver Digital agreed to develop and publish a turn-based role-playing game. The resulting game Serious Sam: The Random Encounter was released on 24 October 2011. During the development of Serious Sam: The Random Encounter, Vlambeer released many small games, most notably Luftrauser. Vlambeer then started development on an iOS version of Radical Fishing called Ridiculous Fishing. Development was halted after a San Francisco-based studio released a clone of Radical Fishing on iOS. This generated discussion about the cloning of video games and led Vlambeer to be somewhat of an icon on the topic. The term "Vlambeer'd" was introduced by several media outlets. In November 2013, the browser game Vlambeer Clone Tycoon was launched to provide a satirical comment on the issue of Vlambeer's cloning ordeals.
             In February 2012, Vlambeer released Gun Godz, a first-person shooter inspired by hip-hop, in collaboration with Brandon Boyer's Venus Patrol. Ridiculous Fishing was nominated for the 2012 Independent Games Festival "Best Mobile" award. At the conference where the award ceremony was held, Vlambeer released Yeti Hunter.
             On 2 December 2012, Vlambeer announced a sequel to Luftrauser called Luftrausers. On 19 December 2012, Vlambeer released the iOS version of Super Crate Box. Its success prevented the studio from going out of business due to the financial ramifications of the decreased motivation caused by the cloning incident. On 14 March 2013, Vlambeer released Ridiculous Fishing on iOS, after resuming its development.</p>
+        <div class="space"></div>
         <h3>Games</h3>
         <ul>
             <li>Super Crate Box (2010)</li>
@@ -19,6 +20,7 @@
             <li>Nuclear Throne (2015)</li>
             <li>Space Murder (in development)</li>
         </ul>
+        <div class="space"></div>
         <div class="space"></div>
 
 @stop
