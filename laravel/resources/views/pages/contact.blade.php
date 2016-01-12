7a8a489e8218a3dc8aff5c7dbbeda86f2a429c27
Damian Leijten <damian@marcleijten.nl>
2016-01-12 11:07:56 +0100

    start on contactpage

--- laravel/resources/views/pages/contact.blade.php
+++ laravel/resources/views/pages/contact.blade.php
@@ -4,6 +4,21 @@
 
     <div class="container">
         <h1>Contact</h1>
+        <div class="col-md-4">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid cumque,
+                delectus deleniti ex fugiat hic mollitia nam quaerat quasi quis reprehenderit
+                sed sint vel! Ab amet doloribus ipsam nam quae!</p>
+        </div>
+        <div class="col-md-4">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid cumque,
+                delectus deleniti ex fugiat hic mollitia nam quaerat quasi quis reprehenderit
+                sed sint vel! Ab amet doloribus ipsam nam quae!</p>
+        </div>
+        <div class="col-md-4">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid cumque,
+                delectus deleniti ex fugiat hic mollitia nam quaerat quasi quis reprehenderit
+                sed sint vel! Ab amet doloribus ipsam nam quae!</p>
+        </div>
     </div>
 
 @stop
