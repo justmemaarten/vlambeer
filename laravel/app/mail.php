344fa823b7a41283ba868c1b09dfc473fa9e2c0b
ateck5 <jitsbuijs@hotmail.com>
2016-01-19 12:21:29 +0100

    mailding voor damian

--- laravel/app/mail.php
+++ laravel/app/mail.php
@@ -6,6 +6,37 @@
  * Time: 11:09
  */
 
+//CODE FOR AUTOMATED MAIL SENDING
+//
+//if(isset($_POST['button_pressed']))
+//{
+//    $to      = 'nobody@example.com';
+//    $subject = 'the subject';
+//    $message = 'hello';
+//    $headers = 'From: webmaster@example.com' . "\r\n" .
+//        'Reply-To: webmaster@example.com' . "\r\n" .
+//        'X-Mailer: PHP/' . phpversion();
+//
+//    mail($to, $subject, $message, $headers);
+//
+//    echo 'Email Sent.';
+//}
+//
+//function sendemail()
+//{
+//    var url = '/mail.php';
+//
+//    new Ajax.Request(url,{
+//            onComplete:function(transport)
+//            {
+//                var feedback = transport.responseText.evalJSON();
+//                if(feedback.result==0)
+//                    alert('There was a problem sending the email, please try again.');
+//            }
+//        });
+//
+//}
+
 if($_POST["message"]) {
     mail("jeroenstamkot@me.com", "Form to email message", $_POST["message"], "From: an@email.address");
 }
